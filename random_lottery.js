13883400f3efbe45810f672b9c441c96af63be25
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-11-21 06:21:30 -0500

    test

--- random_lottery.js
+++ random_lottery.js
@@ -1,3 +1,30 @@
+// FOR WINNING NUMBERS
+var win_5_numbers = prompt("Enter the first 5 winning numbers – the numbers should be entered separated by a space.").split(/[ ,]+/);
+var win_lucky = prompt("Enter the Lucky Ball.");
+
+var win_6_numbers = win_5_numbers;
+
+const check_win_numbers = () => {
+  //sorts win_5_numbers
+  win_5_numbers.sort((num_1, num_2) => (num_1 - num_2));
+
+  //copies the chosen lucky ball to the array win_6_numbers
+  win_6_numbers[5] = win_lucky;
+}
+check_win_numbers();
+
+const win_balls = document.querySelectorAll(".wballs");
+
+//displays the winning numbers
+const display_win_number = () => {
+  for(let col=0; col < 6; col++) {
+    win_balls[col].innerHTML = win_6_numbers[col];
+  }
+}
+display_win_number();
+
+
+// FOR RANDOM NUMBERS
 const total_balls = 6;
 const normal_balls = 5;
 const normal_balls_max = 48;

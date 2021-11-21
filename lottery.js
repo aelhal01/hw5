d66f9a7bd903452d0119aefecb1c844ef08b233c
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-11-20 21:46:52 -0500

    lots

--- lottery.js
+++ lottery.js
@@ -1,17 +1,50 @@
 
-const total_circles = 7;
-const normal_circles = 5;
-const normal_circles_max = 48;
+const total_balls = 7;
+const normal_balls = 5;
+const normal_balls_max = 48;
 const lucky_max = 18;
 
-const lucky_1_index= 5;
-const lucky_2_index= 8;
+const lucky_index= 5;
 
-let random_numbers=[total_circles];
+let random_6_numbers=[total_balls];
 
-const random_circles = document.querySelectorAll(".circles");
+const random_balls = document.querySelectorAll(".balls");
 
 const button_run = document.querySelector("#button_run");
-button_run.addEventListener("click", () => {
-  button_run.innerHTML = "BUTTON CLICKED";
-})
+button_run.addEventListener("click", () => pick_6_numbers());
+
+//picks 1 random number
+//  if the column number is less than 5, then the random number is between 1 and 48
+//  if the column number is 5 or more, then the random number is between 1 and 18
+const pick_1_number = col => {
+  let num;
+  if(col < normal_balls) {
+    num = Math.floor((Math.random() * normal_balls_max) + 1)
+  } else {
+    num = Math.floor((Math.random() * lucky_max) + 1)
+  }
+  return num;
+}
+
+
+//pick all 7 random numbers
+const pick_6_numbers = () => {
+  for (let col = 0; col < total_balls; col++) {
+    random_6_numbers[col] = pick_1_number(col);
+  }
+  check_random_numbers();
+}
+
+
+const check_random_numbers = () => {
+  let random_5_numbers = [normal_balls];
+    //copies 5 numbers from the array random_6_numbers
+  random_5_numbers = random_6_numbers.slice(0, normal_balls);
+
+    //copies 1 lucky ball from the array random_6_numbers
+  let lucky = random_6_numbers[lucky_index];
+}
+
+const display_random_numbers = () => {
+
+}

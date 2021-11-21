10100cd9bbf90eb1c2ea7327277973cd7a37af41
aelhal01 <72872228+aelhal01@users.noreply.github.com>
2021-11-20 19:06:48 -0500

    initial

---
+++ lottery.js
@@ -0,0 +1,10 @@
+
+//generates a random integer between 1 and 48
+function randomint() {
+  var random = Math.floor(Math.random()*48)+1;
+}
+
+//generates a random integer between 1 and 48
+function randomint_lucky() {
+  var random = Math.floor(Math.random()*18)+1;
+}
